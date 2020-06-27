16d842722c23f983b6434ec955a0de98e50e82f7
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2020-06-27 09:45:52 +0200

    Mutation and Query proof

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutation = {};
+const Mutations = {
+    createDog(parent, args, ctx, info) {
+        //Create a dog
+        global.dogs = global.dogs || [];
+        const newDog = { name: args.name };
+        global.dogs.push(newDog);
+        return newDog;
+    }
+};
 
-module.exports = Mutation;
+module.exports = Mutations;
